a57635aa8908d29bc99bba8339cf8b5c31543af7
yaroslav_sagan <yaroslav.sagan@gmail.com>
2023-03-29 00:50:35 +0400

    + problem 26

--- 20_Valid_Parentheses.py
+++ 20_Valid_Parentheses.py
@@ -7,6 +7,21 @@ An input string is valid if:
 Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+Output: false
 """
 
 
