aaf6d60a769a69b43771e8a94592b6a5401f943d
yaroslav_sagan <yaroslav.sagan@gmail.com>
2023-03-28 20:25:17 +0400

    + problem 14
    (also List imported from typing lib where it is needed)

--- 14_Longest_Common_Prefix.py
+++ 14_Longest_Common_Prefix.py
@@ -1,18 +1,45 @@
-from Decorator import decorator
-
-tests = [["flowerkrtzxcv","flowkrtzxcv","flightkrtzxcv"], ["dog","racecar","car"]]
-
-@decorator
-def longest_common_prefix(l: list) -> str:
-    lcs = ""
-    for i in range(len(l)):
-        for j in range(i+1, len(l)):
-            print([ x for x in zip(l[i], l[j]) if x[0] == x[1]])
-    return lcs
+"""
+Write a function to find the longest common prefix string
+amongst an array of strings.
+If there is no common prefix, return an empty string "".
+"""
 
 
+from typing import List
 
-if __name__ == "__main__":
-    for test in tests:
-        pref = longest_common_prefix(test)
-        print("RES: ", pref)
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        """_summary_
+        Runtime 34 ms Beats 72.61% Memory 13.8 MB Beats 79.11%
+        Args:
+            strs (List[str]): List of strings
+
+        Returns:
+            str: the longest common prefix
+        """
+        min_word = min(strs, key=len)
+        for i in range(len(min_word), 0, -1):
+            flag = all([min_word[:i] == word[:i] for word in strs])
+            if flag:
+                return min_word[:i]
+        return ""
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        """_summary_
+        Runtime 35 ms Beats 66.66% Memory 13.9 MB Beats 38.28%
+        Args:
+            strs (List[str]): List of strings
+
+        Returns:
+            str: the longest common prefix
+        """
+        strs.sort()
+        min_word = strs[0]
+        last_word = strs[-1]
+        i = 0
+        while i < len(min_word) and min_word[i] == last_word[i]:
+            i += 1
+        return min_word[:i]
--- Decorator.py
+++ Decorator.py
@@ -5,6 +5,6 @@ def decorator(func):
         start = time()
         result = func(s)
         finish = time() - start
-        print(f'Result: {result}\tTime: {finish*1000000000:.3} ns')
+#        print(f'Result: {result}\tTime: {finish*1000000000:.3} ns')
         return result
     return wrapper
